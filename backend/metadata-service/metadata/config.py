8049175bbbfb8fb08da4447da74b6cd6ff5b9c52
Miguel <miguel.peixoto.99@gmail.com>
2024-02-20 19:16:48 +0000

    feat: Metadata service application

--- backend/metadata-service/metadata/config.py
+++ backend/metadata-service/metadata/config.py
@@ -1,8 +1,4 @@
-""" This script defines global variables that contain the names of the
-Firestore collections and the schemas of the Firestore documents.
-These variables are used for further validation of the data to be
-stored in Firestore.
-"""
+"""Defines global variables for Firestore collections and schemas."""
 
 ## Firestore Collections
 USERS_COLLECTION = "users"
@@ -18,31 +14,26 @@ RENEWAL_DATE_REGEX = r"(0[1-9]|[12][0-9]|3[01])?(/(0[1-9]|1[1,2]))?"
 
 ## Firestore schemas
 CATEGORY_DOCUMENT_SCHEMA = {
-    "name": {
-        "type": "string",
-        "required": True,
-        "regex": CATEGORY_REGEX
-    }
+    "name": {"type": "string", "required": True, "regex": CATEGORY_REGEX}
 }
 
 SUBSCRIPTION_SCHEMAS = {
-    "category": {
-        "type": "string",
-        "regex": CATEGORY_REGEX
-    },
+    "category": {"type": "string", "regex": CATEGORY_REGEX},
     "description": {
         "type": "string",
     },
-    "price": {
-        "type": "number",
-        "regex": PRICE_REGEX
-    },
-    "type": {
-        "type": "string",
-        "regex": TYPE_REGEX
-    },
-    "renewalDate": {
-        "type": "string",
-        "regex": RENEWAL_DATE_REGEX
-    }
+    "price": {"type": "number", "regex": PRICE_REGEX},
+    "type": {"type": "string", "regex": TYPE_REGEX},
+    "renewalDate": {"type": "string", "regex": RENEWAL_DATE_REGEX},
+}
+
+BUDGET_DOCUMENT_SCHEMA = {
+    "budget": {"type": "float", "required": True, "regex": PRICE_REGEX}
+}
+
+PREFERENCES_DOCUMENT_SCHEMA = {
+    "colorPalette": {"type": "string"},
+    "defaultDateRange": {"type": "string", "default": ""},
+    "tableRange": {"type": "number"},
+    "timeSeries": {"type": "string", "regex": TYPE_REGEX},
 }
