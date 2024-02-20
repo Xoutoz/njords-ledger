498ba7e8de1d1a47edacd60186b51481ccb55ee3
Miguel <miguel.peixoto.99@gmail.com>
2024-02-20 19:17:17 +0000

    feat: Metadata service application unit tests

--- backend/metadata-service/tests/constants.py
+++ backend/metadata-service/tests/constants.py
@@ -4,57 +4,85 @@ from google.cloud import firestore
 
 # String constants
 DOCUMENT_ID = "document id"
-COLLECTION = "COLLECTION"
-SUB_COLLECTION = "SUB-COLLECTION"
+COLLECTION = "collection"
+SUB_COLLECTION = "sub-collection"
 CATEGORY_NAME = "category"
 
-DESCRIPTION = "this is a test"
+DESCRIPTION = "description1"
+NEW_DESCRIPTION = "description2"
 SUBSCRIPTION_ID = md5(DESCRIPTION.encode()).hexdigest()
-OLD_SUBSCRIPTION_ID = md5(f"{DESCRIPTION} - old".encode()).hexdigest()
+NEW_SUBSCRIPTION_ID = md5(NEW_DESCRIPTION.encode()).hexdigest()
 
 
 # Input constants
-CATEGORY_INPUT_JSON = {
-    "name": "Health"
-}
+CATEGORY_INPUT_JSON = {"name": CATEGORY_NAME}
+
 SUBSCRIPTION_ALL_REQUIRED_JSON = {
-    "category": "Entertainment",
-    "description": "Disney+",
-    "price": 13.99,
+    "category": CATEGORY_NAME,
+    "description": DESCRIPTION,
+    "price": 1.23,
     "type": "month",
-    "renewalDate": "01"
+    "renewalDate": "01",
 }
+
+NEW_SUBSCRIPTION_WITH_DESCRIPTION_JSON = {
+    "category": CATEGORY_NAME,
+    "description": NEW_DESCRIPTION,
+    "price": 1.23,
+}
+
+NEW_SUBSCRIPTION_WITHOUT_DESCRIPTION_JSON = {"category": CATEGORY_NAME, "price": 1.23}
+
 SUBSCRIPTION_JSON = (
-    {"category": "Entertainment"},
-    {"price": 13.99},
-    {"description": "Disney+"}
+    {"category": CATEGORY_NAME},
+    {"price": 1.23},
+    {"description": DESCRIPTION},
 )
 
 
 # Firestore document constants
 CATEGORY_DOCUMENT = {
     "name": CATEGORY_NAME,
-    "lastModifiedTimestamp": firestore.SERVER_TIMESTAMP
+    "lastModifiedTimestamp": firestore.SERVER_TIMESTAMP,
 }
-SUBSCRIPTION_DOCUMENT = {
-    "description": DESCRIPTION
-}
-UPDATE_SUBSCRIPTION_DOCUMENT = {
-    "category": CATEGORY_NAME
-}
-LISTED_SUBSCRIPTION_DOCUMENT = [{
-    f"{SUBSCRIPTION_ID}": {
-        "description": DESCRIPTION
-    }
-}]
+
+SUBSCRIPTION_DOCUMENT = {"description": DESCRIPTION}
+
+UPDATE_SUBSCRIPTION_DOCUMENT = {"category": CATEGORY_NAME}
+
+LISTED_SUBSCRIPTION_DOCUMENT = [{f"{SUBSCRIPTION_ID}": {"description": DESCRIPTION}}]
 
 
-## Invalid input constants
-INVALID_CATEGORY = "C4r"
-INVALID_CATEGORY_JSON = {
-    "invalid-key": "Health"
-}
+# Invalid input constants
+INVALID_CATEGORY = "category1"
+INVALID_CATEGORY_JSON = {"invalid-key": CATEGORY_NAME}
 INVALID_SUBSCRIPTION = "invalid-id"
-INVALID_SUBSCRIPTION_JSON = {
-    "invalidField": "invalidValue"
+INVALID_SUBSCRIPTION_JSON = {"category": CATEGORY_NAME, "invalidField": "invalidValue"}
+
+
+# Endpoints constants
+USER_ID = "test"
+INEXISTENT_USER_ID = "id"
+NEW_CATEGORY_NAME = CATEGORY_NAME + "-test"
+
+BUDGET_JSON = {"budget": 123}
+
+INVALID_BUDGET_JSON = {"invalid-key": 123}
+
+PREFERENCES_JSON = {
+    "colorPalette": "string",
+    "defaultDateRange": "",
+    "tableRange": 50,
+    "timeSeries": "MONTH",
 }
+
+DOCUMENT_PREFERENCES = {
+    "preferences": {
+        "colorPalette": "string",
+        "defaultDateRange": "",
+        "tableRange": 50,
+        "timeSeries": "MONTH",
+    }
+}
+
+INVALID_PREFERENCES_JSON = {"invalid-key": "invalid-value"}
--- backend/metadata-service/tests/test_operations.py
+++ backend/metadata-service/tests/test_operations.py
@@ -1,6 +1,7 @@
 """Test all Firestore operations"""
 
 
+import pytest
 from tests.constants import (
     DOCUMENT_ID,
     SUB_COLLECTION,
@@ -8,10 +9,13 @@ from tests.constants import (
     CATEGORY_DOCUMENT,
     DESCRIPTION,
     SUBSCRIPTION_ID,
-    OLD_SUBSCRIPTION_ID,
+    NEW_SUBSCRIPTION_ID,
     SUBSCRIPTION_DOCUMENT,
-    UPDATE_SUBSCRIPTION_DOCUMENT,
-    LISTED_SUBSCRIPTION_DOCUMENT
+    NEW_SUBSCRIPTION_WITH_DESCRIPTION_JSON,
+    NEW_SUBSCRIPTION_WITHOUT_DESCRIPTION_JSON,
+    LISTED_SUBSCRIPTION_DOCUMENT,
+    BUDGET_JSON,
+    PREFERENCES_JSON,
 )
 from metadata.operations import (
     user_exists,
@@ -20,144 +24,189 @@ from metadata.operations import (
     document_exists,
     list_categories,
     list_subscriptions,
+    get_budget,
+    get_user_preferences,
     create_category,
     create_subscription,
+    set_budget,
+    set_user_preferences,
     update_category,
     update_subscription,
     remove_category,
     remove_subscription,
+    remove_budget,
     remove_document,
     get_categories_collection,
     get_subscriptions_collection,
     get_subcollection,
     get_collection,
     build_category_document,
-    generate_subscription_id
+    generate_subscription_id,
 )
 from google.cloud import firestore, firestore_v1
 
 
-def test_firestore_fixture(mock_firestore_database):
-    assert isinstance(mock_firestore_database, firestore.Client)
+def test_firestore_fixture(mock_firestore_user_subcollection):
+    assert isinstance(mock_firestore_user_subcollection, firestore.Client)
 
 
-def test_user_exists(mock_firestore_database):
+def test_user_exists(mock_firestore_user_subcollection):
     assert user_exists(DOCUMENT_ID)
 
 
-def test_category_exists(mock_firestore_database):
+def test_category_exists(mock_firestore_user_subcollection):
     assert category_exists(DOCUMENT_ID, DOCUMENT_ID)
 
 
-def test_subscription_exists(mock_firestore_database):
+def test_subscription_exists(mock_firestore_user_subcollection):
     assert subscription_exists(DOCUMENT_ID, DOCUMENT_ID)
     assert subscription_exists(DOCUMENT_ID, SUBSCRIPTION_DOCUMENT)
     assert not subscription_exists(DOCUMENT_ID, [])
 
 
-def test_document_exists(mock_firestore_database):
-    assert document_exists(mock_firestore_database.collection, DOCUMENT_ID)
+def test_document_exists(mock_firestore_user_subcollection):
+    assert document_exists(mock_firestore_user_subcollection.collection, DOCUMENT_ID)
 
 
-def test_list_categories(mock_firestore_list_categories):
-    result = list_categories(DOCUMENT_ID)
+def test_list_categories(mock_firestore_categories_subcollection_with_document):
+    actual_result = list_categories(DOCUMENT_ID)
 
-    assert result
-    assert isinstance(result, list)
-    assert len(result) == 1
-    assert result == [ CATEGORY_NAME ]
-
-
-def test_list_subscriptions(mock_firestore_list_subscriptions):
-    result = list_subscriptions(DOCUMENT_ID)
-    print(result)
-
-    assert result
-    assert result == LISTED_SUBSCRIPTION_DOCUMENT
+    assert isinstance(actual_result, list)
+    assert len(actual_result) == 1
+    assert actual_result == [CATEGORY_NAME]
 
 
-def test_create_category(mock_firestore_database):
-    result = create_category(DOCUMENT_ID, DOCUMENT_ID)
+def test_list_subscriptions(mock_firestore_subscriptions_subcollection_with_document):
+    actual_result = list_subscriptions(DOCUMENT_ID)
 
-    assert result
-    assert result == DOCUMENT_ID
-
-
-def test_create_subscription(mock_firestore_database):
-    result = create_subscription(DOCUMENT_ID, SUBSCRIPTION_DOCUMENT)
-
-    assert result
-    assert result == SUBSCRIPTION_ID
+    assert actual_result
+    assert actual_result == LISTED_SUBSCRIPTION_DOCUMENT
 
 
-def test_update_category(mock_firestore_database):
-    result = update_category(DOCUMENT_ID, f"old_{CATEGORY_NAME}", CATEGORY_NAME)
+def test_get_budget(mock_firestore_user_budget):
+    actual_result = get_budget(DOCUMENT_ID)
 
-    assert result
-    assert result == CATEGORY_NAME
-
-
-def test_update_subscription_with_id(mock_firestore_database):
-    result = update_subscription(DOCUMENT_ID, SUBSCRIPTION_ID, UPDATE_SUBSCRIPTION_DOCUMENT)
-
-    assert result
-    assert result == SUBSCRIPTION_ID
+    assert actual_result
+    assert actual_result == BUDGET_JSON
 
 
-def test_update_subscription(mock_firestore_database):
-    result = update_subscription(DOCUMENT_ID, OLD_SUBSCRIPTION_ID, SUBSCRIPTION_DOCUMENT)
+def test_get_user_preferences(mock_firestore_user_preferences):
+    actual_result = get_user_preferences(DOCUMENT_ID)
 
-    assert result
-    assert result == SUBSCRIPTION_ID
-
-
-def test_remove_category(mock_firestore_database):
-    result = remove_category(DOCUMENT_ID, DOCUMENT_ID)
-
-    assert result
-    assert result == DOCUMENT_ID
+    assert actual_result
+    assert actual_result == PREFERENCES_JSON
 
 
-def test_remove_subscription(mock_firestore_database):
-    result = remove_subscription(DOCUMENT_ID, DOCUMENT_ID)
+def test_create_category(mock_firestore_user_subcollection):
+    actual_result = create_category(DOCUMENT_ID, DOCUMENT_ID)
 
-    assert result
-    assert result == DOCUMENT_ID
-
-
-def test_remove_document(mock_firestore_database):
-    result = remove_document(mock_firestore_database.collection, DOCUMENT_ID)
-
-    assert result
-    assert result == DOCUMENT_ID
+    assert actual_result
+    assert actual_result == DOCUMENT_ID
 
 
-def test_get_categories_collection(mock_firestore_database):
-    result = get_categories_collection(DOCUMENT_ID)
+def test_create_subscription(mock_firestore_user_subcollection):
+    actual_result = create_subscription(DOCUMENT_ID, SUBSCRIPTION_DOCUMENT)
 
-    assert result
-    assert isinstance(result, firestore_v1.collection.CollectionReference)
-
-
-def test_get_subscriptions_collection(mock_firestore_database):
-    result = get_subscriptions_collection(DOCUMENT_ID)
-
-    assert result
-    assert isinstance(result, firestore_v1.collection.CollectionReference)
+    assert actual_result
+    assert actual_result == SUBSCRIPTION_ID
 
 
-def test_get_subcollection(mock_firestore_database):
-    result = get_subcollection(DOCUMENT_ID, SUB_COLLECTION)
+def test_set_budget(mock_firestore_user_budget):
+    try:
+        set_budget(DOCUMENT_ID, BUDGET_JSON["budget"])
+        assert True
+    except Exception:
+        assert False
 
-    assert result
-    assert isinstance(result, firestore_v1.collection.CollectionReference)
+
+def test_set_user_preferences(mock_firestore_user_preferences):
+    try:
+        set_user_preferences(DOCUMENT_ID, PREFERENCES_JSON)
+        assert True
+    except Exception:
+        assert False
 
 
-def test_get_collection(mock_firestore_database):
-    result = get_collection()
+def test_update_category(mock_firestore_user_subcollection):
+    actual_result = update_category(DOCUMENT_ID, f"old_{CATEGORY_NAME}", CATEGORY_NAME)
 
-    assert result
-    assert isinstance(result, firestore_v1.collection.CollectionReference)
+    assert actual_result
+    assert actual_result == CATEGORY_NAME
+
+
+@pytest.mark.parametrize(
+    "document, expected_result",
+    [
+        (NEW_SUBSCRIPTION_WITH_DESCRIPTION_JSON, NEW_SUBSCRIPTION_ID),
+        (NEW_SUBSCRIPTION_WITHOUT_DESCRIPTION_JSON, SUBSCRIPTION_ID),
+    ],
+)
+def test_update_subscription(
+    document, expected_result, mock_firestore_user_subcollection
+):
+    actual_result = update_subscription(DOCUMENT_ID, SUBSCRIPTION_ID, document)
+
+    assert actual_result
+    assert actual_result == expected_result
+
+
+def test_remove_category(mock_firestore_user_subcollection):
+    actual_result = remove_category(DOCUMENT_ID, DOCUMENT_ID)
+
+    assert actual_result
+    assert actual_result == DOCUMENT_ID
+
+
+def test_remove_subscription(mock_firestore_user_subcollection):
+    actual_result = remove_subscription(DOCUMENT_ID, DOCUMENT_ID)
+
+    assert actual_result
+    assert actual_result == DOCUMENT_ID
+
+
+def test_set_budget(mock_firestore_user_budget):
+    try:
+        remove_budget(DOCUMENT_ID)
+        assert True
+    except Exception:
+        assert False
+
+
+def test_remove_document(mock_firestore_user_subcollection):
+    actual_result = remove_document(
+        mock_firestore_user_subcollection.collection, DOCUMENT_ID
+    )
+
+    assert actual_result
+    assert actual_result == DOCUMENT_ID
+
+
+def test_get_categories_collection(mock_firestore_user_subcollection):
+    actual_result = get_categories_collection(DOCUMENT_ID)
+
+    assert actual_result
+    assert isinstance(actual_result, firestore_v1.collection.CollectionReference)
+
+
+def test_get_subscriptions_collection(mock_firestore_user_subcollection):
+    actual_result = get_subscriptions_collection(DOCUMENT_ID)
+
+    assert actual_result
+    assert isinstance(actual_result, firestore_v1.collection.CollectionReference)
+
+
+def test_get_subcollection(mock_firestore_user_subcollection):
+    actual_result = get_subcollection(DOCUMENT_ID, SUB_COLLECTION)
+
+    assert actual_result
+    assert isinstance(actual_result, firestore_v1.collection.CollectionReference)
+
+
+def test_get_collection(mock_firestore_user_subcollection):
+    actual_result = get_collection()
+
+    assert actual_result
+    assert isinstance(actual_result, firestore_v1.collection.CollectionReference)
 
 
 def test_build_category_document():
